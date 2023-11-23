8f535dafdec262327468fcf3eed506b59b3c0c97
kde-la-c <kdelacolinaplant@gmail.com>
2023-11-23 16:45:26 +0100

    fixed norme and simplified code

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,29 +14,27 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
+	t_count	c;
 	char	*ret;
 	size_t	totlen;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
-	j = 0;
+	ft_bzero((void *)&c, sizeof(t_count));
 	totlen = ft_strlen(s1) + ft_strlen(s2);
 	ret = malloc(sizeof(char) * totlen + 1);
 	if (!ret)
 		return (NULL);
-	while (s1[i])
+	while (s1[c.i])
 	{
-		ret[i] = s1[i];
-		i++;
+		ret[c.i] = s1[c.i];
+		c.i++;
 	}
-	while (s2[j])
+	while (s2[c.j])
 	{
-		ret[i + j] = s2[j];
-		j++;
+		ret[c.i + c.j] = s2[c.j];
+		c.j++;
 	}
-	ret[i + j] = 0;
+	ret[c.i + c.j] = 0;
 	return (ret);
 }
