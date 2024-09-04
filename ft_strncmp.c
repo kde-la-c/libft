08ea3e9d682113443d7da444e9c87e55c40f5a05
kde-la-c <kdelacolinaplant@gmail.com>
2024-09-04 21:53:49 +0200

    protected ft_strncmp for null values

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,10 +19,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i2;
 
 	i = 0;
+	if (!n || !s1 || !s2)
+		return (0);
 	i1 = (unsigned char)s1[i];
 	i2 = (unsigned char)s2[i];
-	if (!n)
-		return (0);
 	while (i < n - 1 && s1[i] && s2[i])
 	{
 		if (i1 != i2)
